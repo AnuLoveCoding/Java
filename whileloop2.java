dfc663f270acccb4b38f54cc560a1c119bb7962e
anuLoveCoading <anusrivas07@gmail.com>
2022-10-14 21:00:49 +0530

    whileloop

---
+++ whileloop2.java
@@ -0,0 +1,14 @@
+/**
+ * whileloop2
+ */
+public class whileloop2 {
+
+    public static void main(String[] args) {
+        int n = 100;
+        int i=0;
+        while(n>i){
+            System.out.println(n);
+            n--;
+        }
+    }
+}
