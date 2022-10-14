df5313e34220c9c76ac15e9f1ec2afd6c0076822
anuLoveCoading <anusrivas07@gmail.com>
2022-10-14 12:12:27 +0530

    forLoop

--- forLoop2.java
+++ forLoop2.java
@@ -1,14 +1,24 @@
 class forLoop2{
     public static void main(String[] args) {
-        int n = 20;
+        // int n = 20;
 
-        for (int i = 1; i <= n; i++) {
+        // for (int i = 1; i <= n; i++) {
 
-        if (n % i == 0) {
+        // if (n % i == 0) {
 
-            System.out.print(i + " ");
+        //     System.out.print(i + " ");
 
-        }
-    }
+        // }
+    // }
+
+                int i = 0;
+
+            for(i = 0; i < 10; i++) {
+
+            continue;
+
+            }
+
+            System.out.println(i);
     }
 }
